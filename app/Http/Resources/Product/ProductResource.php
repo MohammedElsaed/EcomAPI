c4dc5d4165e696effd1e27681d2eff65ef717f84
Hash <you@example.com>
2018-05-21 23:02:44 +0200

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -17,11 +17,26 @@ class ProductResource extends JsonResource
         //return parent::toArray($request);
         return [
             'name'          => $this->name,
-            'description'   => $this->detail,
-            'price'         => $this->price,
-            'stock'         => $this->stock,
-            'discount'      => $this->discount
             
+            'description'   => $this->detail,
+            
+            'price'         => $this->price,
+            
+            'stock'         => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            
+            'discount'      => $this->discount,
+            
+                            //Will return Total Price after discount
+            'totalPrice'    => round(( 1 - ($this->discount / 100)) * $this->price , 2),
+            
+            'href'          => [
+                               //Total rating b3d darb 3add al reviews * magmo3 al Stars
+                'rating'    => $this->reviews->count() > 0 ?
+                               round($this->reviews->sum('star') / $this->reviews->count(),2)
+                               : 'No rating yet',
+                
+                'reviews'   => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
