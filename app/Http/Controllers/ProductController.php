3ca6f88ccda6463a758f22df168185948470b050
Hash <you@example.com>
2018-05-23 00:07:17 +0200

    Reviews Resource & Products Paginate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return  Productcollection::collection(Product::all());
+        return  Productcollection::collection(Product::paginate(5));
     }
     
     
